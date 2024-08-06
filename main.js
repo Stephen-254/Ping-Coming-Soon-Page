a49c3c574c9465c2b0666e716b5924ac8968a12a
Stephen-254 <stephenmutheu@gmail.com>
2024-08-06 08:13:52 +0300

    error mesasge done

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 function validateEmail() {
-    const checkIfEmailIsValid = true;
-    const emailRegex = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+
+    
     const emailInput = document.getElementById("email");
     const error = document.getElementById("error")
 
-    if(emailInput == ""){
-        error.style.display = "inline";
+
+    let checkIfEmailIsValid = true;
+
+
+    if(emailInput.value == ""){
+        error.style.display = "block";
         checkIfEmailIsValid = false;
     } else{
         error.style.display = "none";
     }
+
+    /*function ifEmailFieldIsFilled(checkIfEmailIsValid) {
+        const emailRegex = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+
+        if(emailInput) {
+            return emailRegex.test(emailInput);
+        }
+    } */
 
     return checkIfEmailIsValid;
 }
